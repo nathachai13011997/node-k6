5c197bd6787a1e4090a285481af59196d72b80ce
nathachai13011997 <77932024+nathachai13011997@users.noreply.github.com>
2023-06-16 08:29:08 +0700

    ติด tags k6

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,10 @@ import http from "k6/http"
 import { sleep } from "k6"
 
 export default function () {
-  http.get("https://test.k6.io")
+  http.get("https://test.k6.io", {
+    tags: {
+      my_tag: "test"
+    }
+  })
   sleep(1)
 }
