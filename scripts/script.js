4ba792ffce5ab39017d677bdb35aa64ab08c955a
nathachai13011997 <77932024+nathachai13011997@users.noreply.github.com>
2023-06-16 08:36:24 +0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,24 @@
 import http from "k6/http"
 import { sleep } from "k6"
 
-export default function () {
-  http.get("https://test.k6.io", {
-    tags: {
-      my_tag: "test"
+export let options = {
+  tags: {
+    my_tag: "lego"
+  },
+  insecureSkipTLSVerify: true,
+  thresholds: {
+    checks: ["rate>0.94"]
+  },
+  scenarios: {
+    contacts1: {
+      executor: "constant-vus",
+      vus: 1,
+      duration: "1s"
     }
-  })
+  }
+}
+
+export default function () {
+  http.get("https://test.k6.io")
   sleep(1)
 }
