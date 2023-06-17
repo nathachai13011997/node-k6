5986a140285e0852da2b8df65bea83220535929d
nathachai13011997 <77932024+nathachai13011997@users.noreply.github.com>
2023-06-17 22:37:13 +0700

    create function generateTransactionId

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,18 @@
 import http from "k6/http"
 import { sleep } from "k6"
 
+const generateTransactionId = () => {
+  const dateTime = new Date()
+  const dd = String(dateTime.getDate()).padStart(2, "0")
+  const mm = String(dateTime.getMonth() + 1).padStart(2, "0")
+  const yyyy = String(dateTime.getFullYear())
+  return yyyy + mm + dd + Date.now()
+}
+
 export let options = {
   tags: {
-    name: "lego"
+    team: "lego",
+    transaction: generateTransactionId()
   },
   scenarios: {
     contacts1: {
@@ -14,6 +23,7 @@ export let options = {
   }
 }
 
+// k6 run scripts/script.js --out influxdb=http://192.168.1.34:8086/k6
 export default function () {
   http.get("https://test.k6.io")
   sleep(1)
